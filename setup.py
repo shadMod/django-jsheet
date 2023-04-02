ed4cd9ecc87fd6ec10debbee28e273be6a360b1f
mattia allegro <mattia.allegro@yahoo.it>
2023-04-02 13:39:56 +0200

    added setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name="django-jsheet",
+    packages=["django-jsheet"],
+    version="0.1.0",
+    license="MIT License",
+    description="Django JSheet",
+    author="ShadMod",
+    author_email="support@shadmod.it",
+    url="https://github.com/shadMod/django-jsheet",
+    download_url="https://github.com/shadMod/django-jsheet/archive/refs/tags/0.0.1.tar.gz",
+    keywords=[
+        "Django",
+        "JSheet",
+        "Django-JSheet",
+        "Django_JSheet",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
