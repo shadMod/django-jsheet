fb6aa4c03411dc6a0922e82016170e816c744d98
mattia allegro <mattia.allegro@yahoo.it>
2023-06-03 11:59:01 +0200

    fixed imports and added logger and traceback added TIME_UPDATE and SYNC_DB params (with default 10 sec in TIME_UPDATE) reformat init and comment make_data_js() to not prepopulate datajs with DB data fixed dispatch() to:     - add fetch post js file     - save if method is POSY with relative check if result is True (success // ok!) or it's False (look log error) added save() method to populate datajs with the latest changes in make_data_js():     - added data param  to populate file with it (if exists)     - split and added prepopulate_datajs() to prepopulate datajs with DB data added make_fetch_js() to make fetch_post.js to make POST request and save changes in all sheet

--- src/django_jsheet/core/views.py
+++ src/django_jsheet/core/views.py
@@ -1,16 +1,24 @@
 import os
 import json
+import traceback
 
 from datetime import datetime
+from logging import getLogger
 
 from django.conf import settings
+from django.http import HttpResponse
 from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
+logger = getLogger(__name__)
 
 
 class DjangoSheetFormView(FormView):
     HISTORY: bool = True
     LOGROOT: str = None
     SVIL: bool = False
+    TIME_UPDATE: int = 10000  # 10000 => 10 sec
+    SYNC_DB: bool = False
     empty_row: int = 10
     header = None
 
@@ -29,19 +37,51 @@ class DjangoSheetFormView(FormView):
             self.path_filelog, self.filelog = self.mk_file_log()
             self.populate_log()
             self.jspath = self.mk_jspath()
+            # prepopulate js with data model
+            # self.make_data_js()
             # read populate file log
-            self.make_data_js()
             self.jsheadersheet()
+            # populate file log
+            # TODO: code
+            # clean all populate file log (without latest)
+            # TODO: code
 
-    # populate file log
-    # TODO: code
-    # clean all populate file log (without latest)
-    # TODO: code
+    def dispatch(self, *args, **kwargs):
+        # added fetch post file
+        self.make_fetch_js()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["jsheet"] = self.model.objects.all()
-        return context
+        if self.request.method == "POST":
+            res, msg = self.save()
+            if res:
+                result = {
+                    "result": {
+                        "success": msg,
+                    },
+                }
+                json_data = json.dumps(result)
+                return HttpResponse(json_data, content_type="application/json")
+            else:
+                logger.error(msg)
+        return super().dispatch(*args, *kwargs)
+
+    def save(self):
+        try:
+            data_row = json.loads(self.request.body.decode("utf-8"))
+        except Exception as ex:
+            logger.error(traceback.format_exc())
+            return False, ex
+
+        data = []
+        for i, row in data_row.items():
+            data.append(row)
+
+        try:
+            self.make_data_js(data)
+        except Exception as ex:
+            logger.error(traceback.format_exc())
+            return False, ex
+
+        return True, "ok"
 
     def mk_jspath(self):
         jspath = str(self.LOGROOT) + "/jsheet/js/"
@@ -160,15 +200,34 @@ class DjangoSheetFormView(FormView):
         with open(jsfile, "w") as fl:
             fl.write(datajs)
 
-    def make_data_js(self):
+    def make_data_js(self, data: str = None):
         """
         var data = [
-            ['50', 'Robe', 'casa', 'carta', '2019-02-12', 'non pagato'],
-            ['', '', '', '', '', ''],
+            ['50', 'Robe', 'casa', 'carta', '2019-02-12', 'non pagato'], real data value
+            ['', '', '', '', '', ''], empty row (set in init)
         ];
         """
         # init datajs
-        datajs = "var data = ["
+        datajs = "var data = "
+        if not data:
+            # open data
+            datajs += "["
+            # prepopulate datajs if exist model and data row in model
+            datajs += self.prepopulate_datajs()
+            # write empty row
+            for i in range(self.empty_row):
+                datajs += str(["" for x in range(len(self.header))]) + ","
+            # close datajs
+            datajs += "];"
+        else:
+            datajs += str(data)
+
+        jsfile = self.jspath + "/data.js"
+        with open(jsfile, "w") as fl:
+            fl.write(datajs)
+
+    def prepopulate_datajs(self):
+        datajs = ""
         for value in self.model.objects.all():
             list_vl = []
             for key_, field in self.form_class.base_fields.items():
@@ -191,18 +250,8 @@ class DjangoSheetFormView(FormView):
                     list_vl.append(str(vl))
                 if field == "ModelChoiceField":
                     list_vl.append(str(vl))
-
             datajs += str(list_vl) + ","
-
-        # write empty row
-        for i in range(self.empty_row):
-            datajs += str(["" for x in range(len(self.header))]) + ","
-
-        # close datajs
-        datajs += "];"
-        jsfile = self.jspath + "/data.js"
-        with open(jsfile, "w") as fl:
-            fl.write(datajs)
+        return datajs
 
     def populate_log(self):
         datalog = {}
@@ -211,3 +260,69 @@ class DjangoSheetFormView(FormView):
 
         with open(self.filelog, "w") as fl:
             fl.write(json.dumps(datalog))
+
+    def make_fetch_js(self):
+        # get current url and relative path url
+        current_url = self.request.resolver_match.view_name
+        post_url = reverse_lazy(current_url)
+
+        # make fetch post JS
+        fetch_post = """
+        setInterval(function() {get_rows_table()}, %s)
+
+        function get_rows_table() {
+            var t = document.getElementsByClassName('jexcel')[0]
+            var trs = t.getElementsByTagName("tr");
+            var tds = null;
+            
+            var data_j = {};
+            for (var i=1; i<trs.length; i++)
+            {
+                array_data = [];
+                tds = trs[i].getElementsByTagName("td");
+                for (var n=1; n<tds.length;n++)
+                {
+                    array_data.push(tds[n].innerHTML);
+                };
+                data_j[i] = array_data;
+            }
+            update_rows(data_j)
+        }
+        
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        
+        function update_rows(data_j) {
+            fetch("%s", {
+                    method: "POST",
+                    headers: {
+                        "X-CSRFToken": getCookie("csrftoken"),
+                        "Accept": "application/json",
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data_j)
+                }
+            ).then(res => {
+                    <!--console.log("Request complete!");-->
+                }
+            );
+        }
+        """ % (self.TIME_UPDATE, post_url)
+
+        # make and put scripts in fetch_post.js in jspath dir
+        jsfile = self.jspath + "/fetch_post.js"
+        with open(jsfile, "w") as fl:
+            fl.write(fetch_post)
