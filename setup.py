39e1365c8fa43ed4476ab060f7997ab0d2d7d325
mattia allegro <mattia.allegro@yahoo.it>
2023-06-04 18:13:33 +0200

    added download_url

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django_jsheet",
-    version="0.2.3",
+    version="0.2.4",
     author="shadMod",
     author_email="support@shadmod.it",
     description="A little tool to render a simple excel sheet in webpage",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shadMod/django-jsheet",
+    download_url="https://github.com/shadMod/django-jsheet/archive/refs/tags/0.2.4.tar.gz",
     project_urls={
         "Bug Tracker": "https://github.com/shadMod/django-jsheet/issues",
     },
