70879996008cd6fe98aceeef14c222e90f1fc86b
mattia allegro <mattia.allegro@yahoo.it>
2023-06-04 18:00:28 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_jsheet",
+    version="0.2.3",
+    author="shadMod",
+    author_email="support@shadmod.it",
+    description="A little tool to render a simple excel sheet in webpage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shadMod/django-jsheet",
+    project_urls={
+        "Bug Tracker": "https://github.com/shadMod/django-jsheet/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[
+        "django_jsheet", "django_jsheet.src.core", "django_jsheet.src.templatetags",
+    ],
+    python_requires=">=3.7"
+)
