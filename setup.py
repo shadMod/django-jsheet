3204358baeedf259c1a2b38fa296d047e66eeb84
mattia allegro <mattia.allegro@yahoo.it>
2023-09-19 22:52:37 +0200

    fixed setup; set version 0.2.5

--- setup.py
+++ setup.py
@@ -1,31 +1,51 @@
+import os
 import setuptools
+
+__version__ = "0.2.5"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
+def get_install_requires() -> list:
+    """Returns requirements.txt parsed to a list"""
+    fname = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    targets = []
+    if os.path.exists(fname):
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
+
+
 setuptools.setup(
     name="django_jsheet",
-    version="0.2.4",
+    version=__version__,
     author="shadMod",
     author_email="support@shadmod.it",
-    description="A little tool to render a simple excel sheet in webpage",
+    description="Little tools to render a simple excel sheet in webpage",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shadMod/django-jsheet",
-    download_url="https://github.com/shadMod/django-jsheet/archive/refs/tags/0.2.4.tar.gz",
+    download_url=f"https://github.com/shadMod/django-jsheet/archive/refs/tags/{__version__}.tar.gz",
     project_urls={
-        "Bug Tracker": "https://github.com/shadMod/django-jsheet/issues",
+        'Documentation': 'https://docs.shadmod.it/django_jsheet/index',
+        'GitHub': 'https://github.com/shadMod/django-jsheet/',
+        "Bug Tracker": "https://github.com/shadMod/django-jsheet/issues/",
     },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=[
         "django_jsheet", "django_jsheet.src.core", "django_jsheet.src.templatetags",
     ],
-    python_requires=">=3.7"
+    install_requires=get_install_requires(),
+    python_requires=">=3.8"
 )
